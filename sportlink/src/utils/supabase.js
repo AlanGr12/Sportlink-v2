b62bb8ad7693993ec4bdd3402fdd8ff074f00273
ALAN GABRIEL GORDON <48860730@ort>
2026-06-08 12:07:59 -0300

    avz

--- sportlink/src/utils/supabase.js
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-
-const supabaseUrl = "https://cczzvdaraenyqyujbsup.supabase.co"
-;
-const supabaseKey = "sb_publishable_bgIifhu-jA4l5wdIJDzT9w_k-dADfRM";
-
-export const supabase = createClient(supabaseUrl, supabaseKey);
