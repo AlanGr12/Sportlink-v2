d2029659bc0472f89130907997250baec7ba79f1
NICOLAS CUKIER <49006840@ort>
2026-07-15 09:27:56 -0300

    colores arreglados y cosas

--- sportlink/src/iconos/IconoMedalla.jsx
+++ sportlink/src/iconos/IconoMedalla.jsx
@@ -16,3 +16,5 @@ export const IconoMedalla = ({ size = 24, color = "currentColor", className = ""
     <path d="M6 9H4.5a2.5 2.5 0 0 1 0-5H6M18 9h1.5a2.5 2.5 0 0 0 0-5H18M4 22h16M10 14.66V17c0 .55-.45 1-1 1H4v2h16v-2h-5c-.55 0-1-.45-1-1v-2.34M12 2a4 4 0 0 1 4 4v5a4 4 0 0 1-8 0V6a4 4 0 0 1 4-4z"/>
   </svg>
 );
+
+
